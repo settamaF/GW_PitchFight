88246c5095cf5bc66323d1abb88dca9c136f828e
f.settama <f.settama@isartdigital.com>
2015-03-23 14:29:26 +0100

    dll missing

---
+++ UnityProject/PitchFight/Assets/Scripts/GeneratePattern/Generate.cs
@@ -0,0 +1,57 @@
+﻿//******************************************************************************
+// Authors: Frederic SETTAMA  
+//******************************************************************************
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//******************************************************************************
+public class Generate : MonoBehaviour 
+{
+	public enum Pattern
+	{
+		Pattern4x1,
+		Pattern8x1,
+		Pattern16x1
+	}
+#region Script Parameters
+
+#endregion
+
+#region Static
+#endregion
+
+#region Properties
+#endregion
+
+#region Fields
+	// Const -------------------------------------------------------------------
+
+	// Private -----------------------------------------------------------------
+	private List<Transform>	mPatterns;
+#endregion
+
+#region Unity Methods
+	void Start ()
+	{
+
+	}
+	
+	void Update () 
+	{
+
+	}
+#endregion
+
+#region Methods
+
+#endregion
+
+#region Implementation
+	private Transform CreatePatern()
+	{
+
+	}
+#endregion
+}
