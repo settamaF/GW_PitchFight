ef5021a61c8a73921b1652a755d18d9a8f272c93
Blackhart <manceau.thomas19@gmail.com>
2015-03-24 20:22:04 +0100

    Death finish

--- UnityProject/PitchFight/Assets/Scripts/PlayerDeath.cs
+++ UnityProject/PitchFight/Assets/Scripts/PlayerDeath.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerDeath : MonoBehaviour
@@ -6,18 +7,35 @@ public class PlayerDeath : MonoBehaviour
 	#region Public Parameters
 
 	public Camera referenceCamera;
-	public float deathOffset;
+	public RectTransform deathBorder;
+	public GameState gameState;
+
+	#endregion
+
+	#region Private Parameters
+
+	private float __deathBorderRatio;
 
 	#endregion
 
 	#region Unity Callbacks
 
+	private void	Start()
+	{
+		__deathBorderRatio = deathBorder.rect.xMax / 1920.0f;
+	}
+
 	private void	Update()
 	{
-		//if (referenceCamera.WorldToScreenPoint(transform.position).x < deathOffset)
-		//	Debug.Log("Die");
-		//else
-		//	Debug.Log("Alive");
+		Vector3 lWorldToScreenPoint = referenceCamera.WorldToScreenPoint(transform.position);
+		float lXScreenPosPersoRatio = lWorldToScreenPoint.x / Screen.width;
+		float lYScreenPosPersoRatio = lWorldToScreenPoint.y / Screen.height;
+		if (lXScreenPosPersoRatio < __deathBorderRatio || lYScreenPosPersoRatio < 0.0f)
+		{
+			gameState.SetAliveState(transform.GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().playerNumber - 1, false);
+			gameObject.SetActive(false);
+			Destroy(gameObject);
+		}
 	}
 
 	#endregion
