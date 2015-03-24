044e6e92e9790e3c424905c2796fbb9b28424873
jerome <baune45@hotmail.com>
2015-03-24 16:35:51 +0100

    Add modifications on platformer2DUserControl

--- UnityProject/PitchFight/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ UnityProject/PitchFight/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -7,7 +7,7 @@ namespace UnityStandardAssets._2D
     [RequireComponent(typeof (PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
-		public string playerNumber;
+		public int playerNumber;
 
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
@@ -24,8 +24,7 @@ namespace UnityStandardAssets._2D
             if (!m_Jump)
             {
                 // Read the jump input in Update so button presses aren't missed.
-				Debug.Log("player number = " + playerNumber);
-                m_Jump = CrossPlatformInputManager.GetButtonDown(playerNumber + "Jump");
+                m_Jump = CrossPlatformInputManager.GetButtonDown("J" + playerNumber.ToString() + "Jump");
             }
         }
 
@@ -34,7 +33,8 @@ namespace UnityStandardAssets._2D
         {
             // Read the inputs.
             bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = Input.GetAxis(playerNumber + "Horizontal");
+			float h = Input.GetAxis("J" + playerNumber.ToString() + "Horizontal");
+			//Debug.Log("player number = " + playerNumber);
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
             m_Jump = false;
