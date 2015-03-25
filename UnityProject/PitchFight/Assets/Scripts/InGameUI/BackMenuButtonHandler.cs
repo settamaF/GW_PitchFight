2c8dd8bbfeda1426c3bbd11ba17d4e1fc2959262
Blackhart <manceau.thomas19@gmail.com>
2015-03-25 16:04:46 +0100

    Button can now activated with A button pad

---
+++ UnityProject/PitchFight/Assets/Scripts/InGameUI/BackMenuButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackMenuButtonHandler : MonoBehaviour
+{
+	#region Public Parameters
+
+	public GameObject mainMenu;
+	public GameObject game;
+	public GameObject victoryPanel;
+
+	#endregion
+
+	#region Unity Callbacks
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("J1Jump"))
+		{
+			mainMenu.SetActive(true);
+			game.SetActive(false);
+			victoryPanel.SetActive(false);
+		}
+	}
+
+	#endregion
+}
