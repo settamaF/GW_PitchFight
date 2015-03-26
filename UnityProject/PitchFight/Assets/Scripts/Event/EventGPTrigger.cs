aa557c3fa65b0bc2b99f2dfb5e2f6030401fcf61
Blackhart <manceau.thomas19@gmail.com>
2015-03-26 20:39:16 +0100

    Pedago anim added

--- UnityProject/PitchFight/Assets/Scripts/Event/EventGPTrigger.cs
+++ UnityProject/PitchFight/Assets/Scripts/Event/EventGPTrigger.cs
@@ -47,6 +47,7 @@ public class EventGPTrigger : EventTrigger
 	public override void ExecuteEvent()
 	{
 		__gpErrorEventUIHandlerScript.StartUI();
+		DeathBorderUIHandler.LaunchAnimation("Pedago1");
 	}
 
 	public override void EndEvent()
---
+++ UnityProject/PitchFight/Assets/Scripts/InGameUI/DeathBorderUIHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathBorderUIHandler : MonoBehaviour
+{
+	#region Private Parameters
+
+	private static Animator __animator;
+
+	#endregion
+
+	#region Unity Callbacks
+
+	private void	Start()
+	{
+		__animator = GetComponent<Animator>();
+	}
+
+	#endregion
+
+	#region Actions
+
+	public static void	LaunchAnimation(string pAnimName)
+	{
+		__animator.Play(pAnimName);
+	}
+
+	#endregion
+}
