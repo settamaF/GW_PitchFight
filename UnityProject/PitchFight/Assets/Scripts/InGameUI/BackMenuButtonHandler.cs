20dc99911947d56fb1ed0c999c8505dccaca1de6
Blackhart <manceau.thomas19@gmail.com>
2015-03-26 15:33:25 +0100

    Timer added to VictoryPanel

--- UnityProject/PitchFight/Assets/Scripts/InGameUI/BackMenuButtonHandler.cs
+++ UnityProject/PitchFight/Assets/Scripts/InGameUI/BackMenuButtonHandler.cs
@@ -9,18 +9,36 @@ public class BackMenuButtonHandler : MonoBehaviour
 	public GameObject game;
 	public GameObject victoryPanel;
 
+	public float delayBeforeActivateInput;
+
+	#endregion
+
+	#region Private Parameters
+
+	private float __currentDelay;
+
 	#endregion
 
 	#region Unity Callbacks
 
 	private void Update()
 	{
-		if (Input.GetButtonDown("J1Jump"))
+		__currentDelay += Time.deltaTime;
+		if (Input.GetButtonDown("J1Jump") && __currentDelay >= delayBeforeActivateInput)
 		{
 			mainMenu.SetActive(true);
 			game.SetActive(false);
 			victoryPanel.SetActive(false);
 		}
+	}
+
+	#endregion
+
+	#region Actions
+
+	public void	ActiveUI()
+	{
+		__currentDelay = 0.0f;
 	}
 
 	#endregion
