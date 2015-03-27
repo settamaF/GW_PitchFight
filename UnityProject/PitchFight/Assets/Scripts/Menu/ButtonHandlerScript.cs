395e1a00c5556958c6fdd06c63b168dbbb1babb7
Blackhart <manceau.thomas19@gmail.com>
2015-03-27 10:34:24 +0100

    MenuHandler done

---
+++ UnityProject/PitchFight/Assets/Scripts/Menu/ButtonHandlerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ButtonHandlerScript : MonoBehaviour
+{
+	#region Public Parameters
+
+	public GameObject deselectObject;
+	public GameObject selectObject;
+
+	#endregion
+
+	#region Actions
+
+	public void	Click()
+	{
+		selectObject.SetActive(true);
+		deselectObject.SetActive(false);
+	}
+
+	#endregion
+}
